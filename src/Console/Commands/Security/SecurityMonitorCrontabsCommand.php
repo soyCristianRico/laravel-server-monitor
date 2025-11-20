bd6418bfba29aec18765b2136b720cea160b6051
Cristian <cristiancr.rico@gmail.com>
2025-11-20 12:02:04 +0100

    Rollback log debug

--- src/Console/Commands/Security/SecurityMonitorCrontabsCommand.php
+++ src/Console/Commands/Security/SecurityMonitorCrontabsCommand.php
@@ -19,7 +19,7 @@ class SecurityMonitorCrontabsCommand extends Command
         $this->initializeNotificationService();
         $scanner = app(SecurityScannerService::class);
 
-        logger()->debug('Monitoring crontab modifications...');
+        $this->info('Monitoring crontab modifications...');
 
         $alerts = [];
 
@@ -38,7 +38,7 @@ class SecurityMonitorCrontabsCommand extends Command
             return 1;
         }
 
-        logger()->debug('✅ No recent crontab modifications detected');
+        $this->info('✅ No recent crontab modifications detected');
 
         return 0;
     }
